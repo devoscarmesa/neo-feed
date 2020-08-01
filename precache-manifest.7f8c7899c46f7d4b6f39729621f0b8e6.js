f0f1612b7324489040ef11cdcb2053f3a821219c
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-08-01 03:45:54 -0500

    Updates

---
+++ precache-manifest.7f8c7899c46f7d4b6f39729621f0b8e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2324ac13eda531666cd8879024cf7c",
+    "url": "/neo-feed/index.html"
+  },
+  {
+    "revision": "2cb5de3f1096ef1b921b",
+    "url": "/neo-feed/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "367af1afbfe2171c12af",
+    "url": "/neo-feed/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2cb5de3f1096ef1b921b",
+    "url": "/neo-feed/static/js/2.e519355d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/neo-feed/static/js/2.e519355d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367af1afbfe2171c12af",
+    "url": "/neo-feed/static/js/main.de76cc96.chunk.js"
+  },
+  {
+    "revision": "266d5acfd014a735dec7",
+    "url": "/neo-feed/static/js/runtime-main.f414d352.js"
+  }
+]);
